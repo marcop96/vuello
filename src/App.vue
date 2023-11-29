980e851702c2111cf02095299ca9bc59c36a3c86
“Marco <marco.pianaroli96@gmail.com>
2023-11-29 17:36:50 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="bg-red h-50px w-screen flex items-center justify-center w-1 text-white">TROLO</nav>
+
+  <main grid grid-cols-4>
+    <div w- bg-red h-screen shadow-xl shadow-black rounded p-3 items-center justify-center>DASHBOARD</div>
+    <div>COLUMNA 2</div>
+    <div>COLUMNA 2</div>
+    <div>COLUMNA 2</div>
+  </main>
+</template>
