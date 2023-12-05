9a939971960ec5da69b43a5ee9981bc1363c67a0
“Marco <marco.pianaroli96@gmail.com>
2023-12-05 17:13:12 +0100

    dynamically changes background based on completion

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -13,7 +13,7 @@ function viewTask() {
 </script>
 
 <template>
-  <div class="bg-blue h-max rounded-5 shadow-sm text-center ">
+  <div :class="!task.completed ? 'bg-red' : 'bg-green'" class="h-max rounded-5 shadow-sm text-center">
     <h3 hover:cursor-pointer hover:bg-blue-5 hover:rounded-xl @click="viewTask">{{ task.title }}</h3>
     <p>
       {{ task.id }}
