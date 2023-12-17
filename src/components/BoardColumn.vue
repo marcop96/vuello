e02d053c02b92317a0c68df7f7bb129d801329f7
“Marco <marco.pianaroli96@gmail.com>
2023-12-17 11:02:21 +0100

    specifies function name to make it understandable

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useTasks } from '@store/useTasks'
 import { ref } from 'vue'
+import { useFocus } from '@vueuse/core'
 import TaskCard from '@/components/TaskCard.vue'
 import type { Column } from '@/types'
 import { useColumns } from '@/store/useColumns'
@@ -9,10 +10,10 @@ const props = defineProps<{
   column: Column
 }>()
 const { addTask } = useTasks()
-const newTaskTitle = ref('')
-const editTitle = ref(false)
 const { updateTitle } = useColumns()
+const newTaskTitle = ref('')
 const columnTitle = ref('')
+const editableColumnTitle = ref(false)
 
 function createTask() {
   if (newTaskTitle.value === '')
@@ -20,26 +21,24 @@ function createTask() {
   addTask(newTaskTitle.value, props.column.id)
   newTaskTitle.value = ''
 }
-function updateTitleHandler() {
-  editTitle.value = !editTitle.value
+function updateColumnTitleHandler() {
+  editableColumnTitle.value = !editableColumnTitle.value
   if (columnTitle.value === '')
     return
 
   updateTitle(props.column.id, columnTitle.value)
   columnTitle.value = ''
-  // when user presses enter key update title
-  // when user clicks outside of input update title
 }
 </script>
 
 <template>
   <main class="h-max w-40 m-4 p-2 rounded-5 bg-red shadow-xl shadow-sm text-center">
-    <div v-if="editTitle" w-full flex>
-      <input v-model="columnTitle" :placeholder="columnTitle" class="w-full bg-transparent text-center" @keydown.enter="updateTitleHandler">
+    <div v-if="editableColumnTitle" w-full flex>
+      <input v-model="columnTitle" :placeholder="columnTitle" class="w-full bg-transparent text-center" @keydown.enter="updateColumnTitleHandler">
       <button>&#10004; </button>
     </div>
     <div v-else w-full flex justify-between>
-      <h2 class="w-full text-center text-2xl" @click="updateTitleHandler">{{ props.column.title }}</h2>
+      <h2 class="w-full text-center text-2xl" @click="updateColumnTitleHandler">{{ props.column.title }}</h2>
     </div>
     <div id="tasks">
       <TaskCard v-for="task in column.tasks" :key="task.id" :task="task" :column-id="props.column.id" />
